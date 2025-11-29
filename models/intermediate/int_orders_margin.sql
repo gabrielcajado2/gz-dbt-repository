6e123ff8ad1dcdd6c7639723c543f1b39e3dd36a
gabrielcajado2@gmail.com <gabrielcajado2@gmail.com>
2025-11-29 20:24:44 +0000

    2. Calculate the Margin per Order

---
+++ models/intermediate/int_orders_margin.sql
@@ -0,0 +1,9 @@
+select 
+orders_id,
+min(date_date) as date_date,
+sum(revenue) as revenue,
+sum(quantity) as quantity,
+sum(purchase_cost) as purchase_cost,
+sum(margin) as margin
+from {{ ref('int_sales_margin') }}
+group by orders_id
